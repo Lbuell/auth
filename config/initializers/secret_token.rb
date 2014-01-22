753042606ca4a5f4be835618165a787f1470a3aa
lbuell <liamcbuell@gmail.com>
2014-01-22 07:40:03 -0800

    Railscast auth from scratch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auth::Application.config.secret_token = '182a3f0ae4c7d3c70160ad096ba3db987ebc2da725e74c01b9b9706a69ff1db87149c24bb8e3107edbef1078e571d6a2189a549edc98dca1968eeba341aa5ac8'
